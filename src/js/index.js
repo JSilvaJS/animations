93961e136220dbaeff8366a8c213b4b38ca2638c
JONATHAN SILVA <silva.jon86@gmail.com>
2016-03-08 11:56:13 -0500

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,34 @@
+// Javascript Entry Point
+
+import $ from 'jquery';
+
+$('body').css('background', 'red');
+
+$('.hide').on('click', function() {
+	$('img').toggleClass('hidden');
+});
+
+$('.slide').on('click', function() {
+	$('.app').toggleClass('slid');
+});
+
+//event delegation without jquery...
+
+// var buttonList = document.querySelector('.button-list');
+
+// buttonList.addEventListener('click', function(event){
+
+// 	//event.currentTarget === the button-list
+// 	//event.target === the actual button that was clicked
+// 	var letter = event.target.textContent;
+
+// 	alert(letter);
+
+// });
+
+//event delegation WITH jQuery
+
+$('.button-list').on('click', 'button', function(e){
+	var letter = event.target.innerHTML;
+	alert(letter);
+});
